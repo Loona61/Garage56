d2e37fc7f7fe59c67b8bf28169524c8d47cce2db
Loona61 <aur6l@free.fr>
2016-05-07 17:19:55 +0200

    clean + fix color
    organisation dans dossier + fix color bouton alert

--- js/script.js
+++ js/script.js
@@ -7,12 +7,12 @@ function Person(firstName, lastName, age , sex) {
     this.able = function(firstName, lastName, age, sex) {
         sexe = (this.sex == 'm') ? 'il':'elle';
         tooOld = (this.age > 40 || this.age < 18 ) ? ' et '+ sexe +' n\'a pas l\'age requis puisqu\''+ sexe +' a ':' malgré un age cohérent puisqu\''+ sexe +' a ';
-        unique = (this.age > 1) ? ' Ans. ':' An. ';
+        unique = (this.age > 1) ? ' ans. ':' an. ';
         swal('Oups ...', this.firstName + ' ' + this.lastName + ' n\'est pas un bon choix pour cette formation' + tooOld + this.age + unique, 'error');
     };
     
     this.winner = function(firstName, lastName, age, sex) {
-        unique = (this.age > 1) ? ' Ans. ':' An. ';
+        unique = (this.age > 1) ? ' ans. ':' an. ';
         swal('Bien joué !', this.firstName + ' ' + this.lastName + ' est le choix parfait pour cette formation, et je suis sur que vous n\'avez pas hesité une seconde, en plus il a l\'age parfait: ' + this.age + unique, 'success');
         document.getElementsByClassName("choice")[0].innerHTML = this.firstName + ' a été choisi. Appelez le vite pour le prevenir, il n\'attend que ca.';
         document.getElementById("winner").style.display = 'inline';
